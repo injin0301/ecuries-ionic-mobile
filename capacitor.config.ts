a3bdab301a5f06ee50f0c2e6bed1901d2c483c8d
IKIM <Ecris ton email ici, en gardant les quotes>
2022-04-06 09:33:25 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ecurie-de-persevere-mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
